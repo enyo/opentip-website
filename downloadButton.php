92b518294b2fa72d3c556d9270271ff7545d25bf
Matthias Loitsch <m@tthias.com>
2011-02-08 15:03:34 +0100

    Updated download button.

--- downloadButton.php
+++ downloadButton.php
@@ -1,1 +1,1 @@
-<a ot="You know you want to..." ot-delay="5" target="_blank" name="download" class="download" href="http://github.com/enyo/opentip/"><span class="big">Download Opentip</span><span class="version">1.2.11</span><span class="date">Feb, 03 2011</span></a>
+<a ot="You know you want to..." ot-delay="5" target="_blank" name="download" class="download" href="http://github.com/enyo/opentip/"><span class="big">Download Opentip</span><span class="version">1.3.0</span><span class="date">Feb, 08 2011</span></a>
